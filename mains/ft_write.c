45cea057e2dcb788601e403a6e7f41926569b480
Teyber <boucettaghali@gmail.com>
2020-07-02 20:29:06 +0200

    repairing errno

--- mains/ft_write.c
+++ mains/ft_write.c
@@ -6,7 +6,7 @@
 /*   By: gboucett <gboucett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/15 16:05:02 by gboucett          #+#    #+#             */
-/*   Updated: 2020/03/15 19:16:50 by gboucett         ###   ########.fr       */
+/*   Updated: 2020/07/02 18:35:07 by gboucett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,13 @@ int		main(int ac, char **av)
 {
 	char *str = (ac > 1) ? av[1] : "Hello world !\n";
 
-	int fd = open("bonjour", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	// int fd = open("bonjour", O_CREAT | O_WRONLY | O_TRUNC, 0000);
+	int fd = 55521;
 	printf("fd = %d\n", fd);
-	if (fd < 0)
-	{
-		perror("error : ");
-		exit(1);
-	}
-	printf("-- ret value : %ld", ft_write(fd, str, strlen(str)));
+	ssize_t result = ft_write(fd, str, strlen(str));
+	printf("-- ret value : %ld\n", result);
+	if (result < 0)
+		perror("error");
 	close(fd);
 	return (0);
 }
